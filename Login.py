c9e6c4d0599866065d4bc528d6a30551887f368d
Shrouk Hegazi <67985725+shroukhegazi@users.noreply.github.com>
2021-09-04 19:17:36 +0200

    Add files via upload

--- Login.py
+++ Login.py
@@ -9,21 +9,18 @@ def loginf ():
     mail= input ("Enter Your email:")
     your_password = input ("Enter Your Password:")
     
-
-
-    print(type(mydata))
     importusers = open("users.json")
-    print(type(importusers))
-    print(importusers.read())
+    # print(type(importusers))
+    # print(importusers.read())
     users = json.load(importusers)
-    print("11")
+    print("before loop")
     for line in users:
-        print("22")
+        print("through the  first loop")
         Dict = json.loads(line)
         list.append(Dict)
    
     for dict in list:
-        print("hiii")
+        print("hiii from inside the second loop")
         if (mail == dict ['email'] )and (your_password == dict['password']):
             print("login successful")
             user_id = dict['user_id']
@@ -58,28 +55,4 @@ loginf()
         
 
    
-    # if ((mail == Registration.registerf.email) and (your_password== Registration.registerf.password)):
-    #     print (" welcom to your account")
-    #     choice = input ("enter c for create or v for view or e for edit or d for delet or s for search:")
-    #     if (choice == 'c'):
-    #         import CreatePost
-    #         CreatePost.create_post()
-    #     elif (choice == 'v'):
-    #         import view_posts
-    #         view_posts.view_posts()
-    #     elif (choice == 'e'):
-    #         import edit_post
-    #         edit_post.edit_post()
-    #     elif (choice == 'd'):
-    #         import delete_post
-    #         delete_post.delete_post()
-    #     elif (choice == 's'):
-    #         import search_posts
-    #         search_posts.search_posts()  
-    #     else:
-    #         input("enter c for create or v for view or e for edit or d for delet or s for search:")
-    # else :
-    #     print("Re-enter your info:")
-
- 
-
+   
